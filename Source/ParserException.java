67ed325446a77c5e87e811c70354722ef4ab71e3
PierrickJACQUETTE <pierrick.jacquette@gmail.com>
2016-04-06 18:57:07 +0200

    Cette classe renvoi les erreurs du <b>Parser</b> .

---
+++ Source/ParserException.java
@@ -0,0 +1,11 @@
+/**
+ * Cette classe renvoi les erreurs du <b>Parser</b> .
+ * @author ELBEZ Samuel & JACQUETTE Pierrick & PERRACHON Quentin
+ */
+public class ParserException extends RuntimeException {
+	
+	public ParserException(String s){
+		super(s);
+	}
+	
+}
